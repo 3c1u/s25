b2dfefc66a9a3463fb782fe871a94954a3eb7c83
Hikaru Terazono (3c1u) <3c1u@vulpesgames.tokyo>
2020-11-23 20:55:59 +0900

    s25viewer-react: fix lint

--- s25viewer-react/src/components/molecules/LayerList.tsx
+++ s25viewer-react/src/components/molecules/LayerList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Collapse, Drawer, List } from '@material-ui/core'
+import { Drawer, List } from '@material-ui/core'
 import { useDispatch } from 'react-redux'
 
 import LayerItem from '~/components/molecules/LayerItem'
